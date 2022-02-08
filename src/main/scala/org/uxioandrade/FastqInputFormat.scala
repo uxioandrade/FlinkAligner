d39a6e84159bc98ef7739fed4d6d88d2d0d4f285
Uxio Garcia Andrade <uxiog21@gmail.com>
2022-02-09 00:20:41 +0100

    Add Fastq input format reader

--- src/main/scala/org/uxioandrade/FastqInputFormat.scala
+++ src/main/scala/org/uxioandrade/FastqInputFormat.scala
@@ -1,9 +1,34 @@
 package org.uxioandrade
 
 import org.apache.flink.api.common.io.FileInputFormat
+import org.apache.flink.core.fs.FileInputSplit
 
-class FastqInputFormat extends FileInputFormat{
+import java.io.{BufferedInputStream, BufferedReader, FileInputStream, InputStreamReader}
+import java.util.zip.GZIPInputStream
 
+case class Sequence(identifier: String, letters: String, quality: String)
 
+class FastqInputFormat(path: String) extends FileInputFormat[Sequence]{
+
+  var br: BufferedReader = _
+  var end = false
+
+  override def open(fileSplit: FileInputSplit): Unit = {
+    val gzip = new GZIPInputStream(new BufferedInputStream(new FileInputStream(path)))
+    br = new BufferedReader(new InputStreamReader(gzip))
+  }
+
+  override def nextRecord(reuse: Sequence): Sequence = {
+    val id = br.readLine()
+    val seq = br.readLine()
+    val opt = br.readLine()
+    val quality = br.readLine()
+    end = id == null
+    end match {
+      case false => Sequence(id, seq, quality)
+      case _ => null
+    }
+  }
+  override def reachedEnd(): Boolean = end
 
 }
