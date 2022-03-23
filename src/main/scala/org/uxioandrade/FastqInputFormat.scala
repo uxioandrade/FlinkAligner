49c1564f096dca860f4f3d94aac72e5de7c385e9
Uxio Garcia Andrade <uxiog21@gmail.com>
2022-03-23 10:04:46 +0100

    Decouple input case classes

--- src/main/scala/org/uxioandrade/FastqInputFormat.scala
+++ src/main/scala/org/uxioandrade/FastqInputFormat.scala
@@ -1,17 +1,17 @@
 package org.uxioandrade
 
 import org.apache.flink.api.common.io.FileInputFormat
+import org.apache.flink.api.java.functions.KeySelector
 import org.apache.flink.core.fs.FileInputSplit
 
 import java.io.{BufferedInputStream, BufferedReader, FileInputStream, InputStreamReader}
 import java.util.zip.GZIPInputStream
 
-case class Sequence(identifier: String, letters: String, quality: String)
-
 class FastqInputFormat(path: String) extends FileInputFormat[Sequence]{
 
   var br: BufferedReader = _
   var end = false
+  var keyCount = 0
 
   override def open(fileSplit: FileInputSplit): Unit = {
     val gzip = new GZIPInputStream(new BufferedInputStream(new FileInputStream(path)))
@@ -23,9 +23,11 @@ class FastqInputFormat(path: String) extends FileInputFormat[Sequence]{
     val seq = br.readLine()
     val opt = br.readLine()
     val quality = br.readLine()
+    val key = keyCount
+    keyCount = (keyCount + 1) % 5
     end = id == null
     end match {
-      case false => Sequence(id, seq, quality)
+      case false => Sequence(key, id, seq, quality)
       case _ => null
     }
   }
